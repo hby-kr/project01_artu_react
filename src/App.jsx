81d53b7151576cfb7aa55658520e43977567cabf
HBY <hbigpillar@gmail.com>
2025-05-20 16:26:48 +0900

    tailwind 세팅

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {useState} from 'react'
+import './styles/App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+   const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Art U 시작</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+   return (
+      <>
+         <div>
+            <h1>Art U 시작</h1>
+            <div className="min-h-screen flex justify-center items-center bg-blue-100">
+               <h1 className="text-sm font-bold underline">
+                  Hello world!
+               </h1>
+            </div>
+
+         </div>
+         <div className="card">
+            <button onClick={() => setCount((count) => count + 1)}>
+               count is {count}
+            </button>
+         </div>
+      </>
+   )
 }
 
 export default App
